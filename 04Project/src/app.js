6dea742f22e75890ac227254b81deb3fa9d7717e
Ashish Shah <eiepixxstuff@gmail.com>
2024-09-12 11:50:14 +0530

    write routes and conrtollers

--- 04Project/src/app.js
+++ 04Project/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'  // CRUD operation on cookie of user browser
+const app = express();
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -14,6 +15,18 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
-const app = express();
+// import routes
+
+import userRouter from './routes/user.routes.js'
+
+
+
+// routes declartion
+app.use('/api/v1/users', userRouter)
+
+
+//http://localhost:4000/api/v1/users/(variable path)
+
+
 
 export default app
